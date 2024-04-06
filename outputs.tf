2222414d3d8266ccef41a617b4ce2427ac58521c
EC2 Default User <ec2-user@ip-172-31-36-119.us-east-2.compute.internal>
2024-04-06 00:16:55 +0000

    added required tf files

---
+++ outputs.tf
@@ -0,0 +1,10 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+output "domain-name" {
+  value = aws_instance.web.public_dns
+}
+
+output "application-url" {
+  value = "${aws_instance.web.public_dns}/index.php"
+}
---
+++ terraform.tf
@@ -0,0 +1,27 @@
+
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+
+  /* Uncomment this block to use Terraform Cloud for this tutorial
+  cloud {
+      organization = "organization-name"
+      workspaces {
+        name = "learn-terraform-*"
+      }
+  }
+  */
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
